6bc9436bf3d4fa0a74f44e6cb1eaeaedcf6e86b0
frankito42 <francisco.goonzalez99@gmail.com>
2022-02-09 22:24:27 -0300

    d

--- moduloVentas/js/ventas.js
+++ moduloVentas/js/ventas.js
@@ -219,7 +219,7 @@ $(document).ready(function(){
                     </div>`
               }
                 
-
+ 
                 
 
             document.getElementById("clientes").innerHTML=elementos
